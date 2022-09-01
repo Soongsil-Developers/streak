987aba2b9896cd0e358c5de923fd8ac04b3e8de2
JunSeo <oponize@naver.com>
2022-09-01 21:51:21 +0900

    feat: 데이터 가공 로직 - 3

--- packages/core/src/utils/streakHelper.ts
+++ packages/core/src/utils/streakHelper.ts
@@ -1,11 +1,22 @@
 import { eachDayOfInterval } from 'date-fns';
+import dayjs from 'dayjs';
+import type { MapDate, SortingDateProps } from '../types';
 
 const streakHelper = {
   1: (start: globalThis.Date | number, end: globalThis.Date | number) => {
     return eachDayOfInterval({ start, end });
   },
   2: () => {},
-  3: () => {},
+  sortingDate: (dates: SortingDateProps[]) => {
+    const map = new Map<string, MapDate[]>();
+    dates.forEach((date) => {
+      const key = dayjs(date.date).format('YYYYMMDD');
+      const value: MapDate = { type: date.type, amount: date.amount };
+      if (map.has(key)) map.get(key)?.push(value);
+      else map.set(key, [value]);
+    });
+    return map;
+  },
   4: () => {},
   5: () => {},
 };
