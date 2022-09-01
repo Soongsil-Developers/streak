9b32df1c1bb1f8468ccca9eee889d997318e8658
leehyewon0531 <ifpffk@naver.com>
2022-09-01 22:05:45 +0900

    feat: sumAmountByType 함수 추가

--- packages/core/src/utils/streakHelper.ts
+++ packages/core/src/utils/streakHelper.ts
@@ -1,12 +1,40 @@
 import { eachDayOfInterval } from 'date-fns';
 
+interface dateObject {
+  type: string | number;
+  amount: number;
+}
+
 const streakHelper = {
   1: (start: globalThis.Date | number, end: globalThis.Date | number) => {
     return eachDayOfInterval({ start, end });
   },
   2: () => {},
   3: () => {},
-  4: () => {},
+  sumAmountByType: (mapByDate: Map<string, Array<dateObject>>) => {
+    const newMap = new Map<string, Array<dateObject>>()
+
+    mapByDate.forEach((value, key, map) => {
+      newMap.set(key, [])
+
+      value.forEach((obj) => {
+        let summedUp = false
+
+        newMap.get(key)?.forEach((compareObj) => {
+          if(obj.type === compareObj.type) {
+            compareObj.amount += obj.amount
+            summedUp = true
+          } 
+        })
+
+        if(summedUp === false) {
+          newMap.get(key)?.push(obj)
+        }
+      })
+
+    })
+    return newMap
+  },
   5: () => {},
 };
 
