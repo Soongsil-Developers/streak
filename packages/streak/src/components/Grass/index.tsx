866ae5bb49d06b9335076cd02f395221b009bda9
leehyewon0531 <50830078+leehyewon0531@users.noreply.github.com>
2022-09-13 17:38:39 +0900

    feat: 잔디밭 UI 구현 (#28)
    * feat: 잔디밭 UI 구현
    
    * feat: 잔디밭 UI 구현
    
    * fix: 폰트 크기, 간격 수정
    
    * fix: 테스트문 제거, useMemo() 코드 추가
    
    * refactor: useMemo() 구조 수정

--- packages/streak/src/components/Grass/index.tsx
+++ packages/streak/src/components/Grass/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import createDate, { SortingDateProps, ResultDate } from '../../utils/core';
 
 export interface Props {
@@ -8,7 +8,7 @@ export interface Props {
 const Grass: React.FC<Props> = ({ data }) => {
   let result = new Map<string, ResultDate[]>();
 
-  const getStreakHelperResult = () => {
+  const getStreakHelperResult = useMemo(() => {
     const end = new Date();
     const start = new Date(
       new Date().setFullYear(
@@ -19,34 +19,61 @@ const Grass: React.FC<Props> = ({ data }) => {
     );
 
     result = createDate(start, end, data);
-    console.log(result);
 
     const array = Array.from(result, ([date, value]) => ({ date, value }));
-    console.log(array);
     return array;
-  };
+  }, [data]);
 
   const getGrassColor = (length: number) => {
     switch (true) {
       case length === 0:
-        return { fill: 'grey', opacity: '0.2' };
+        return 'hsl(0, 0%, 80%)';
 
       case length > 0 && length <= 2:
-        return { fill: 'green', opacity: '0.25' };
+        return 'hsl(0, 100%, 85%)';
 
       case length > 2 && length <= 6:
-        return { fill: 'green', opacity: '0.4' };
+        return 'hsl(0, 100%, 75%)';
 
       case length > 6 && length <= 15:
-        return { fill: 'green', opacity: '0.6' };
+        return 'hsl(0, 100%, 65%)';
 
       case length > 15 && length <= 22:
-        return { fill: 'green', opacity: '1' };
+        return 'hsl(0, 100%, 50%)';
 
       case length > 22:
-        return { fill: 'red', opacity: '1' };
+        return 'hsl(210, 100%, 70%)';
     }
   };
+
+  const getMonth = (month: String) => {
+    switch(month) {
+      case '01':
+        return 'JAN';
+      case '02':
+        return 'FEB';
+      case '03':
+        return 'MAR';
+      case '04':
+        return 'APR';
+      case '05':
+        return 'MAY';
+      case '06':
+        return 'JUN';
+      case '07':
+        return 'JUL';
+      case '08':
+        return 'AUG';
+      case '09':
+        return 'SEP';
+      case '10':
+        return 'OCT';
+      case '11':
+        return 'NOV';
+      case '12':
+        return 'DEC';
+    }
+  }
 
   const isMonthStart = (date: String) => {
     if (date.substring(date.length - 2) === '01') return true;
@@ -54,9 +81,60 @@ const Grass: React.FC<Props> = ({ data }) => {
   };
 
   const clickDay = (e: React.MouseEvent<HTMLElement>) => {
-    if (e.target instanceof Element)
+    if (e.target instanceof Element) {
       result.get(e.target.id)?.map((item) => console.log(item.type));
+    }
   };
+
+  const createGrass = () => {
+    const range = getStreakHelperResult;
+    let rectArr: React.ReactNode[] = [];
+    let textArr: React.ReactNode[] = [];
+    let x = 10;
+    let y = 10;
+    let width = 15;
+    let height = 15;
+    let count = 0; // 7일마다 다음 줄로 넘어가게 해주기 위한 변수
+
+    range.forEach((el, index) => {
+      if(count % 7 === 0) {
+        x += 20;
+      }
+
+      rectArr.push(
+        React.createElement(
+          'rect',
+          { 
+            width, height,
+            x, y: y + (20 * (count % 7)),
+            rx: 4, ry: 4,
+            fill: getGrassColor(el.value.length),
+            strokeWidth: 2.5,
+            stroke: "#fff",
+            key: index,
+            id: el.date,
+            onClick: clickDay,
+          }
+        )
+      );
+
+      if(isMonthStart(el.date)) {
+        textArr.push(
+          <text x={x} y={163} fontSize={14} key={el.date} >
+            {getMonth(el.date.substring(
+              el.date.length - 4,
+              el.date.length - 2
+            ))}
+          </text>
+        )
+      };
+
+      count += 1;
+    })
+
+    const svgArr: React.ReactNode = React.createElement('svg', { width: 2000, height: 200, background: "#fff" }, [...rectArr, ...textArr])
+    return svgArr;
+  }
 
   return (
     <>
@@ -68,30 +146,8 @@ const Grass: React.FC<Props> = ({ data }) => {
           flexWrap: 'wrap',
         }}
       >
-        {getStreakHelperResult().map((data, index) => (
-          <span key={index} onClick={clickDay}>
-            <svg style={{ width: '25', height: '25' }}>
-              <rect
-                id={data.date}
-                width={20}
-                height={20}
-                rx="5"
-                ry="5"
-                style={getGrassColor(data.value.length)}
-              />
-              {isMonthStart(data.date) && (
-                <text x="2" y="15" fontSize={13}>
-                  {data.date.substring(
-                    data.date.length - 4,
-                    data.date.length - 2
-                  )}
-                </text>
-              )}
-            </svg>
-          </span>
-        ))}
+      {createGrass()}
       </div>
-      <div></div>
     </>
   );
 };
